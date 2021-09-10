c8fd483571ac911bbd5460eb42c29145f74fc99d
Mu Wei Kuo <kmw03062@hotmail.com>
2021-09-10 15:29:27 +1000

    Basic input form construct

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,23 @@ import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
     return <form>
-
+        <div className="new-expense__controls">
+            <div className="new-expense__control">
+                <label>Title</label>
+                <input type="text"></input>
+            </div>
+            <div className="new-expense__control">
+                <label>Amount</label>
+                <input type="number" min="0.01" step="0.01"></input>
+            </div>
+            <div className="new-expense__control">
+                <label>Date</label>
+                <input type="date" min="2020-01-01" max="2022-01-01"></input>
+            </div>
+        </div>
+        <div className="new-expense__actions">
+            <button type="submit">Add expense</button>
+        </div>
     </form>
 };
 
