b5a5a31d1359d08dad3d12dcc7005455c931e537
Mu Wei Kuo <kmw03062@hotmail.com>
2021-10-11 16:22:22 +1000

    Use styled component package to replace custom react UI components

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,11 +1,17 @@
-import React from 'react';
+// import React from 'react';
+import styled from 'styled-components';
 
-import './Card.css'
+const Card = styled.div`
+    border-radius: 12px;
+    box-shadow: 0 1px 8px rgba(0, 0, 0, 0.25);
+`;
 
-const Card = props => {
-    return (
-        <div className={'card ' + props.className}>{props.children}</div>
-    )
-}
+// import './Card.css'
+
+// const Card = props => {
+//     return (
+//         <div className={'card ' + props.className}>{props.children}</div>
+//     )
+// }
 
 export default Card;
